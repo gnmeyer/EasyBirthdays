e1bd1be22f2bd5fe0ea8d2646f92254ee2af05f5
Grant Meyer <gmeyer1@csuchico.edu>
2023-04-21 14:45:57 -0700

    unstable

--- easy_birthdays/lib/create.dart
+++ easy_birthdays/lib/create.dart
@@ -1,79 +1,132 @@
-import 'package:flutter/material.dart';
-import 'package:easy_birthdays/main.dart';
-import 'package:easy_birthdays/storage.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:html';
 
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
-class CreateRoute extends StatelessWidget {
-  const CreateRoute({super.key});
+class CreateRoute extends StatefulWidget {
+  const CreateRoute({Key? key}) : super(key: key);
+
+  @override
+  _CreateRouteState createState() => _CreateRouteState();
+}
+
+class _CreateRouteState extends State<CreateRoute> {
+  final _formKey = GlobalKey<FormState>();
+  final _eventStorage = FirebaseFirestore.instance;
+
+  //Text field
+  final _eventBudgetController = TextEditingController();
+  final _eventTitleController = TextEditingController();
+  final _eventDateController = TextEditingController();
+  final _eventShopController = TextEditingController();
+
+  late String name;
+  late String pass;
+
+  void submitForm() async {
+    name = _eventTitleController.text;
+    pass = _eventDateController.text;
+    String eventTitle = _eventTitleController.text;
+    double eventBudget = double.tryParse(_eventBudgetController.text) ?? 0;
+    DateTime eventDate = DateTime.parse(_eventDateController.text);
+    Map<String, double> eventShop = _eventShopController.text
+        .split(',')
+        .asMap()
+        .map((index, value) =>
+            MapEntry('Item ${index + 1}', double.parse(value)));
+
+    if (_formKey.currentState!.validate()) {
+      final eventData = {
+        'name': name,
+        'pass': pass,
+      };
+      await _eventStorage.collection('events').add(eventData);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create'),
-      ),
-      body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        MyCustomForm(),
-      ])),
-    );
-  }
-}
-
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-// Define a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormState extends State<MyCustomForm> {
-  final EventStorage _storage = EventStorage();
-  late Future<String> _username;
-  final _usernameController = TextEditingController();
-
-  Future<void> _setName() async {
-    final String username = _usernameController.text;
-    await _storage.writeUserInfo(username);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Test"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
+        appBar: AppBar(title: Text('create event')),
+        body: Center(
+            child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              // Add TextFormFields and ElevatedButton here.
+              TextFormField(
+                controller: _eventTitleController,
+                decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Enter your username',
-                  hintText: 'username'),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  _setName();
+                  hintText: 'Enter an event name',
+                ),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an event name';
+                  }
+                  return null;
                 },
-                child: const Text('Submit')),
-          ],
-        ),
-      ),
-    );
+              ),
+              TextFormField(
+                controller: _eventDateController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter an event date',
+                ),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a date';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _eventShopController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter an item name',
+                ),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an item name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _eventBudgetController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter event budget',
+                ),
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a number';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Creating Event')),
+                    );
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        )));
   }
 }
--- easy_birthdays/lib/events.dart
+++ easy_birthdays/lib/events.dart
@@ -3,6 +3,9 @@ import 'package:easy_birthdays/create.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_birthdays/main.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
 class EventsRoute extends StatelessWidget {
   const EventsRoute({super.key});
 
@@ -14,6 +17,36 @@ class EventsRoute extends StatelessWidget {
       ),
       body: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('events').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Loading...');
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['eventTitle']),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Date: ${data['eventDate']}'),
+                      Text('Budget: ${data['eventBudget']}'),
+                      Text('Shop: ${data['eventShop']}'),
+                    ],
+                  ),
+                );
+              }).toList(),
+            );
+          },
+        ),
         ElevatedButton(
           onPressed: () {
             // Navigate to edit page
@@ -29,7 +62,7 @@ class EventsRoute extends StatelessWidget {
             // Navigate to edit page
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const CreateRoute()),
+              MaterialPageRoute(builder: (context) => CreateRoute()),
             );
           },
           child: const Text('Create'),
